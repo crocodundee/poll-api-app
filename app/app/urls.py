06ad262e6cd307551f25294184b2a69f29c83a4e
Anastasia Didan <anastasia.didan.asm@gmail.com>
2020-05-17 12:56:00 +0100

    API documentation

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
+
+
+API_TITLE = 'Poll API'
+API_DESC = 'A Web API for the passage of polls'
+schema_view = get_schema_view(title=API_TITLE)
+
 
 urlpatterns = [
+    path('', include_docs_urls(title=API_TITLE, description=API_DESC)),
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
-    path('api/poll/', include('poll.urls'))
+    path('api/poll/', include('poll.urls')),
+    path('schema/', schema_view)
 ]
